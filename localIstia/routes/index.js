bdc686822238fc177088635c5f4d354e2974f8ac
afunk <alice.funk@consertotech.pro>
2018-03-08 17:47:14 +0100

    Ajout de la première fonction qui pourrait faire quelque chose -> le addMarker

--- localIstia/routes/index.js
+++ localIstia/routes/index.js
@@ -6,6 +6,16 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'LOCALISTIA' });
+
+  $('#addMarkerForm').submit(function(event){
+    event.preventDefault();
+    socket.emit('addMarker', {
+      firstname  : $('#firstname').val(),
+      name  : $('#name').val(),
+      email : $('#email').val()
+    });
+  });
+
 });
 
 module.exports = router;
