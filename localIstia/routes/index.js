a2b7cb8c45e0a7da93a10c0e6f008c6c52b2b509
afunk <alice.funk@consertotech.pro>
2018-03-08 15:20:50 +0100

    Ajout de la route spécifique à about, index.js ne doit servir que pour les traitement en lien avec la page html index

--- localIstia/routes/index.js
+++ localIstia/routes/index.js
@@ -8,9 +8,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'LOCALISTIA' });
 });
 
-/* GET about page. */
-router.get('/about', function(req, res, next) {
-  res.render('about');
-});
-
 module.exports = router;
