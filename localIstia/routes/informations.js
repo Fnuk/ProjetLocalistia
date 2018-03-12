2273bc3d6e9e45a64d8f24987b4ec1527cdcc838
afunk <alice.funk@consertotech.pro>
2018-03-12 16:43:19 +0100

    ajout du package nodegeocoder pour recuperer lat et long d'une adresse + formulaire et enregistrement des données opérationnel + redirection sur page de remerciement

--- localIstia/routes/informations.js
+++ localIstia/routes/informations.js
@@ -1,48 +1,63 @@
 var express = require('express');
 var router = express.Router();
-//pour récuperer la lat et lng depuis l'adresse renseignée
-var geocoder;
+//pour récuperer la lat et lng depuis l'adresse renseignée utilisation de node-geocoder (middleware avec google api)
+var NodeGeocoder = require('node-geocoder');
 
-//initialisation du geocoder
-function initialize() {
-    geocoder = new google.maps.Geocoder();
-}
+//option du geocoder
+var options = {
+ provider: 'google',
+
+ // Optional depending on the providers
+ httpAdapter: 'http', // Default
+ apiKey: 'AIzaSyC1gihQv-kHjLscCQABx7eESNzuNijIYXo', // for Mapquest, OpenCage, Google Premier
+ formatter: null // 'gpx', 'string', ...
+};
+
+//le geocoder
+var geocoder = NodeGeocoder(options);
 
 /* GET informations page. */
 router.get('/', function(req, res, next) {
-  res.render('informations');
+
+
+ res.render('informations');
+
 });
 
 router.post('/sendInfos', function(req, res, next) {
-  var db = req.db;
+ var dbinfo = req.db.collection('informations');
+ var dbmarker = req.db.collection('markerCollection');
 
-  //initialize le géocoder
-  initialize();
-  
-  //on géolocalise le lieu via l'adresse
-  var address = req.body.inputAdresse+" "+req.body.inputCity+" "+req.body.inputCountry;
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == 'OK') {
-          db.markerCollection.insert({"lat":results[0].geometry.location.lat, "long":results[0].geometry.location.lng});
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
+ //on géolocalise le lieu via l'adresse
+ var address = req.body.inputAdresse + " " + req.body.inputCity + " " + req.body.inputCountry;
+ geocoder.geocode(address)
+  .then(function(result) {
+   console.log(result);
+   dbmarker.insert({
+    "lat": results[0].latitude,
+    "long": results[0].longitude
+   });
+  })
+  .catch(function(status) {
+   console.log('Geocode was not successful for the following reason: ' + status);
+  });
 
-  //ici inserer les données récupérée du formulaire
-  db.informations.insert({"adresse": req.body.inputAdresse,
-                          "ville": req.body.inputCity, 
-                          "codePostal": req.body.inputZip, 
-                          "pays": req.body.inputCountry,
-                          "goodDeals": req.body.goodDeals,
-                          "devises": req.body.devises,
-                          "coutVie": req.body.gridRadios,
-                         "contact": req.body.contactMe});
-  res.redirect('/informations/thanks');
+ //ici inserer les données récupérée du formulaire
+ dbinfo.insert({
+  "adresse": req.body.inputAdresse,
+  "ville": req.body.inputCity,
+  "codePostal": req.body.inputZip,
+  "pays": req.body.inputCountry,
+  "goodDeals": req.body.goodDeals,
+  "devises": req.body.devises,
+  "coutVie": req.body.gridRadios,
+  "contact": req.body.contactMe
+ });
+ res.redirect('/informations/thanks');
 });
 
-router.post('/informations/thanks', function(req, res, next) {
-  res.render('thanks');
+router.get('/thanks', function(req, res, next) {
+ res.render('thanks');
 });
 
 module.exports = router;
